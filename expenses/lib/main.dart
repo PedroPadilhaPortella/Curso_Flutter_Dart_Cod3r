159ab808f0f99fe1d9962444332ff5f5d86bc89f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-10-03 01:29:36 -0300

    criando as transacoes de teste

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(ExpensesApp());
+main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
@@ -14,25 +15,67 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(id: 't1', title: 'SSD 250GB', value: 390, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Aula Ingles', value: 70, date: DateTime.now()),
+  ];
+
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas Pessoais'),
+        title: const Text('Despesas Pessoais'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('Gráfico'),
-            ),
+          const Card(
+            child: Text('Gráfico'),
           ),
-          Card(
-            child: Text('Lista de Transações'),
+          Column(
+            children: <Widget>[
+              ..._transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        child: Text(
+                          transaction.value.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                              fontSize: 20),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text(
+                            transaction.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            transaction.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList()
+            ],
           )
         ],
       ),
