fca26ac7faa01481ef58cd042b7761587b3d16c6
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-10-15 01:58:30 -0300

    componentização de funcionalidades flutter

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:expenses/components/transaction_user.dart';
 
-main() => runApp(const ExpensesApp());
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,101 +13,27 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transactions = [
-    Transaction(id: 't1', title: 'SSD 250GB', value: 390, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Aula Ingles', value: 70, date: DateTime.now()),
-  ];
-
-  MyHomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Despesas Pessoais'),
+        title: Text('Despesas Pessoais'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const Card(
-            child: Text('Gráfico'),
-          ),
-          Column(
-            children: <Widget>[
-              ..._transactions.map((transaction) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        child: Text(
-                          'R\$ ${transaction.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple,
-                              fontSize: 20),
-                        ),
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Text(
-                            transaction.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat('dd/MMM/yyyy').format(transaction.date),
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }).toList()
-            ],
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(labelText: 'Título'),
-                  ),
-                  TextField(
-                      controller: valueController,
-                      decoration: InputDecoration(labelText: 'Valor (R\$)')),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.purple,
-                          ),
-                          // ignore: avoid_print
-                          onPressed: () => print(
-                              {titleController.text, valueController.text}),
-                          child: Text('Nova Transação')),
-                    ],
-                  )
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
               ),
             ),
-          )
-        ],
+            TransactionUser(),
+          ],
+        ),
       ),
     );
   }
