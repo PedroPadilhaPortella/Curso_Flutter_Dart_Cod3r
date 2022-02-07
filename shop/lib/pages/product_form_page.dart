a3b9a0c505c0edb9ee06a1377473ed5c029f33d8
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-02-07 00:48:20 -0300

    tratamento de erros ao salvar

--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -71,7 +71,19 @@ class _ProductFormPageState extends State<ProductFormPage> {
 
     Provider.of<ProductList>(context, listen: false)
         .saveProduct(_formData)
-        .then((value) {
+        .catchError((error) {
+      return showDialog<void>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text("Ocorreu um erro."),
+                content: const Text("Ocorreu um erro ao salvar o produto."),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("Ok"))
+                ],
+              ));
+    }).then((value) {
       setState(() => _isLoading = false);
       Navigator.of(context).pop();
     });
