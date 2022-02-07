1570f8102c7d8eef2c8838b814d6beb09bc13650
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-02-07 01:00:48 -0300

    async await e try catch

--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -35,8 +35,8 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  Future<void> addProduct(Product product) {
-    final future = http.post(
+  Future<void> addProduct(Product product) async {
+    final response = await http.post(
       Uri.parse('$baseUrl/products.json'),
       body: jsonEncode({
         "name": product.name,
@@ -47,18 +47,17 @@ class ProductList with ChangeNotifier {
       }),
     );
 
-    return future.then<void>((response) {
-      final id = jsonDecode(response.body)['name'];
-      _items.add(Product(
-        id: id,
-        name: product.name,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        isFavorite: product.isFavorite,
-      ));
-      notifyListeners();
-    });
+    final id = jsonDecode(response.body)['name'];
+    _items.add(Product(
+      id: id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+      isFavorite: product.isFavorite,
+    ));
+
+    notifyListeners();
   }
 
   Future<void> updateProduct(Product product) {
--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -60,7 +60,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
     setState(() {});
   }
 
-  void submitForm() {
+  Future<void> submitForm() async {
     final bool isValid = _formKey.currentState?.validate() ?? false;
 
     if (!isValid) return;
@@ -69,10 +69,12 @@ class _ProductFormPageState extends State<ProductFormPage> {
 
     setState(() => _isLoading = true);
 
-    Provider.of<ProductList>(context, listen: false)
-        .saveProduct(_formData)
-        .catchError((error) {
-      return showDialog<void>(
+    try {
+      await Provider.of<ProductList>(context, listen: false)
+          .saveProduct(_formData);
+      Navigator.of(context).pop();
+    } catch (error) {
+      await showDialog<void>(
           context: context,
           builder: (ctx) => AlertDialog(
                 title: const Text("Ocorreu um erro."),
@@ -83,10 +85,9 @@ class _ProductFormPageState extends State<ProductFormPage> {
                       child: const Text("Ok"))
                 ],
               ));
-    }).then((value) {
+    } finally {
       setState(() => _isLoading = false);
-      Navigator.of(context).pop();
-    });
+    }
   }
 
   bool isValidImageUrl(String url) {
